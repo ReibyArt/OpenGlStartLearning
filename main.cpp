b3b243cae63a3bd0ccb37e39f8a7347a3826e958
ReibyArt <kushinartiom92@gmail.com>
2024-11-07 23:29:21 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main() {
+	/*std::cout << "HellO!";*/
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLReibyArtGames", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create a window GLFW!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLAD
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+		std::cout << "Failed To Initialize GLAD!" << std::endl;
+		return -1;
+	}
+	// Call Functions // 
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+
+// Size of Screen
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0,0,width,height);
+}
